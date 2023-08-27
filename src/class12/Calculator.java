125a355ab62f4456b26c5cdaa9d7046870e83ad7
salih3298 <salih3298@gmail.com>
2023-08-27 18:34:50 +0300

    k

---
+++ src/class12/Calculator.java
@@ -0,0 +1,19 @@
+package class12;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public void add() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the first number");
+        int num1 = scanner.nextInt();
+        System.out.println("Please enter second number");
+        int num2 = scanner.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Sum of these numbers is " + sum);
+    }
+
+
+}
+

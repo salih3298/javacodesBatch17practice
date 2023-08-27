27139f77012e2d78d6415baa89fe64c9bfc9d871
salih3298 <salih3298@gmail.com>
2023-08-27 20:59:07 +0300

    k

---
+++ src/class12/T2Methods.java
@@ -0,0 +1,15 @@
+package class12;
+
+public class T2Methods {
+
+    public void largest(int num1, int num2) {
+        if (num1 > num2) {
+            System.out.println(num1 + "is larger");
+        } else {
+            System.out.println(num2 + " is larger");
+        }
+
+    }
+
+}
+
---
+++ src/class12/T2MethodsTester.java
@@ -0,0 +1,11 @@
+package class12;
+
+public class T2MethodsTester {
+
+    public static void main(String[] args) {
+        T2Methods t2 = new T2Methods();
+        t2.largest(10, 87);
+    }
+
+}
+

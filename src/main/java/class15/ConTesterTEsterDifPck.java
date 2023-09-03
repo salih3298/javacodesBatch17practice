b9c57f0b3cdb95ac23ac760bd2b8de0fcbaf5911
salih3298 <salih3298@gmail.com>
2023-09-03 19:37:44 +0300

    GitHub

---
+++ src/main/java/class15/ConTesterTEsterDifPck.java
@@ -0,0 +1,12 @@
+package class15;
+
+import class16.ConTester;
+
+public class ConTesterTEsterDifPck {
+    public static void main(String[] args) {
+        //   new ConTester();
+        // new ConTester("dnfdkj");
+        new ConTester(15);
+        new ConTester(10.52155);
+    }
+}

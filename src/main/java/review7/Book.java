890a132e4e1cf6ed659b12ad4c1f82ebaa7410fc
salih3298 <salih3298@gmail.com>
2023-09-08 10:15:33 +0300

    GitHub

--- src/main/java/review7/Book.java
+++ src/main/java/review7/Book.java
@@ -3,37 +3,37 @@ package review7;
 public class Book {
 
     /* syntax of Variables
-        dataType name
+        dataType name;
      */
     String title;
     String author;
     int year;
     double price;
 
-    /* syntax of method
+    static int totalBooks;
+    static String type="AudioBook";
 
-    returnType name
-    void read()
+    /*  syntax of method
 
-    I can create my own methods.:
-    1. with parameters or without
-    2. with return or without
+        returnType name()
+        void read()
+
+        I can create my own methods:
+            1. with parameters or without
+            2. with return or without
      */
 
     void read() {
-        System.out.println("I am reading " + title + " book by " + author);
+        System.out.println("I am reading " +title +" book by "+author);
     }
 
-    void changePrice(int newPrice) {
-        System.out.println("The price of the book " + title + " was changed from " + price + " to " + newPrice);
+    void changePrice(int newPrice){
+        System.out.println("The price of the book "+title+" was changed from "+price +" to "+newPrice);
     }
 
-  double totalWithTax(double tax) {
+    double totalWithTax(double tax) {
         double total=price+(price*tax);
         return total;
-  }
-
-
-
+    }
 
 }

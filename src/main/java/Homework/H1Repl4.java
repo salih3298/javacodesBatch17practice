c46e41f9107243299b6eb3d8398b396a64a45454
salih3298 <salih3298@gmail.com>
2023-09-10 16:28:43 +0300

    GitHub

---
+++ src/main/java/Homework/H1Repl4.java
@@ -0,0 +1,20 @@
+package Homework;
+
+public class H1Repl4 {
+    public static void main(String[] args) {
+/* Print out the following pattern:
+Output:
+*
+**
+***
+##
+#  */
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("##");
+        System.out.println("#");
+
+    }
+}
---
+++ src/main/java/Homework/H1Repl8.java
@@ -0,0 +1,16 @@
+package Homework;
+
+public class H1Repl8 {
+    public static void main(String[] args) {
+/* Create 2 Strings
+Use the concatenation operation to make the following output
+**Output:**
+syntaxtechs.com */
+
+        String var1 = "syntaxtechs";
+        String var2 = ".com";
+        System.out.println(var1 + var2);
+
+
+    }
+}

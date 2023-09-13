61ce0a09190f7b7658e5d55e2802789746608922
salih3298 <salih3298@gmail.com>
2023-09-14 02:00:54 +0300

    GitHub

---
+++ src/main/java/Homework/H1Repl35.java
@@ -0,0 +1,19 @@
+package Homework;
+
+public class H1Repl35 {
+    public static void main(String[] args) {
+  /*For you to do:**
+   Create a program that prompt user with question:  "Do you need a loan?"(Use a boolean)
+   If the result is true then prompt user with question: "What is your credit score?". Otherwise eligibility is "Unknown"
+   Based on the score define users eligibility:
+   - if score is below  600 --> eligibility = "Not eligible"
+   - if score is between 600 and 700 inclusive -->  eligibility = "Maybe eligible"
+   - if score is between 701 and 800 inclusive -->  eligibility = "Eligible"
+   - if score is higher than any other previous values --> eligibility = "Definitely eligible" .
+   **Output:**
+     The eligibility is ____*/
+
+
+
+    }
+}

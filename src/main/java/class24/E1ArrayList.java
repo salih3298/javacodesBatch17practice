26d1bb9320842858a9af3e4d37711d44ee9fb24f
salih3298 <salih3298@gmail.com>
2023-09-17 22:08:58 +0300

    GitHub

--- src/main/java/class24/E1ArrayList.java
+++ src/main/java/class24/E1ArrayList.java
@@ -4,18 +4,22 @@ import java.util.ArrayList;
 
 public class E1ArrayList {
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<>();
+
+        ArrayList<String> names=new ArrayList<>();
         names.add("Deepali");
         names.add("Ahmed");
         names.add("Mike");
         names.add("John");
         names.add("Ali");
-        //names.remove(0);
+        // names.remove(0);
         names.remove("Deepali");
         System.out.println(names);
-        names.add(2, "Ehab");
+        names.add(2,"Ehab");
         System.out.println(names);
-        names.set(1, "Jamel");
+        names.set(1,"Jamel");
         System.out.println(names);
+
+
+
     }
 }

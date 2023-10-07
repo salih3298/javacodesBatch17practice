ccd1e53fde7199bd49fc07b0c44150a03df27e70
salih3298 <salih3298@gmail.com>
2023-10-07 18:10:45 +0300

    GitHub

---
+++ src/main/java/class29/E2Exceptions.java
@@ -0,0 +1,7 @@
+package class29;
+
+public class E2Exceptions {
+    public static void main(String[] args) {
+        System.out.println(12);
+    }
+}
---
+++ src/main/java/class29/E3Exceptions.java
@@ -0,0 +1,9 @@
+package class29;
+
+public class E3Exceptions {
+    public static void main(String[] args) {
+
+        System.out.println("a");
+        System.out.println(10/0);
+    }
+}

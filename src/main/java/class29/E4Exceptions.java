3bf87d5d0f066b899ebcab23e78c4f458cdabab5
salih3298 <salih3298@gmail.com>
2023-10-07 18:21:15 +0300

    GitHub

---
+++ src/main/java/class29/E4Exceptions.java
@@ -0,0 +1,15 @@
+package class29;
+
+import class23.BankAccount;
+
+public class E4Exceptions {
+    public static void main(String[] args) {
+
+        // int[] arr=new int[-5];
+        // System.out.println(10/0);
+        // store nothing in bankAccount variable
+        BankAccount bankAccount = null;
+        bankAccount.getBankAccountNumber();
+
+    }
+}

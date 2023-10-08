9e47bcf62644ee4087b9cac67d0c5b7cd00e88c9
salih3298 <salih3298@gmail.com>
2023-10-08 15:54:01 +0300

    GitHub

--- src/main/java/Replit/H1Repl209.java
+++ src/main/java/Replit/H1Repl209.java
@@ -34,11 +34,16 @@ call above method with these arguments.
 ```*/
 public class H1Repl209 {
 
-   public static Map<String,Integer> removeValues(Map<String,Integer> map) {
+ /*   public static Map<String, Integer> removeValues(Map<String, Integer> map) {
 
-       //if ("kjh",5);
-        return map;
+        map.forEach((k, v) -> {
+            if (v > 15) {
+                map.remove(k);
+            }
+        });
+    return map;
     }
+
     public static void main(String[] args) {
 
         Map<String, Integer> map = new HashMap<>();
@@ -48,11 +53,27 @@ public class H1Repl209 {
         map.put("apple", 30);
 
         map.put("orange", 20);
-
-        removeValues(map);
+        System.out.println(removeValues(map));
+        //removeValues(map);
     }
 
 
+} */
+
+    public Map<String, Integer> removeValues(Map<String, Integer> d) {
+        d.entrySet().removeIf(x -> x.getValue() > 15);
+        return d;
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> maps = new HashMap<>();
+        maps.put("mango", 10);
+        maps.put("apple", 30);
+        maps.put("orange", 20);
+
+        H1Repl209 c = new H1Repl209();
+        c.removeValues(maps);
+        maps.entrySet().forEach(x -> System.out.println(x.getKey() +
+                " : " + x.getValue()));
+    }
 }
-
-
